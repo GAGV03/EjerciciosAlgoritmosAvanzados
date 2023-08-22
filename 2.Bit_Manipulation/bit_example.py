1ae7f12033b41c4e0821fdebdf497c43f7111a67
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2023-08-22 10:27:00 -0600

    Terminando Bit Manipulation

--- 2.Bit_Manipulation/bit_example.py
+++ 2.Bit_Manipulation/bit_example.py
@@ -40,6 +40,68 @@ print(f'{x >> 1 = }')
 print(f'{x >> 3 = }')
 
 def is_even(n : int) -> bool:
-    return(n & 1) == 0
+    return (n & 1) == 0
+
+def is_power_of_2(n:int) -> bool:
+    return (n & (n - 1)) == 0
+
+def twos_complement(n: int):
+    return (~n) + 1
+
+def binary(n: int):
+    if n == 0:
+        return '0'
+    result: list[str] = []
+    while n:
+        result.append(str(n & 1))
+        # if(n & 1) == 1:
+        #     result.append('1')
+        # else:
+        #     result.append('0')
+        n >>= 1
+    return ''.join(result[::-1])
 
 print(f'{is_even(5) = }')
+print(f'{is_power_of_2(4) = }')
+print(f'{is_power_of_2(4096) = }')
+print(f'{is_power_of_2(666) = }')
+print(f'{twos_complement(5) = }')
+print(f'{twos_complement(-42) = }')
+print(f'{binary(5) = }')
+print(f'{binary(10) = }')
+print(f'{binary(0) = }')
+print(f'{binary(42) = }')
+print(f'{binary(1024) = }')
+
+#value swapping
+
+#Using a temporary
+x = 5
+y = 8
+
+print(f'{x = }, {y = }')
+t = x
+x = y
+y = t
+
+print(f'{x = }, {y = }')
+
+#Using parallel assignment (destructuring)
+
+x = 5
+y = 8
+
+print(f'{x = }, {y = }')
+
+x,y = y,x
+
+print(f'{x = }, {y = }')
+
+#XOR value swapping
+x = 5
+y = 8
+print(f'\n{x = },{y = }')
+x = x ^ y
+y = x ^ y
+x = x ^ y
+print(f'{x = }, {y = }')
