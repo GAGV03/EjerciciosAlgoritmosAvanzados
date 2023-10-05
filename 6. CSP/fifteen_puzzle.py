b96d1897cfb946e9de5038d9e606b1bd0957cded
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2023-10-05 09:00:48 -0600

    solucion metodo heuristic

--- 6. CSP/fifteen_puzzle.py
+++ 6. CSP/fifteen_puzzle.py
@@ -28,12 +28,23 @@ def successors(frame: Frame) -> list[Frame]:
     return []
 
 def heuristic(frame: Frame) -> float:
-    return 1.0
+    count = 0.0
+    for tup, tup2 in zip (goal,frame):
+        for elem,elem2 in zip (tup,tup2):
+            if elem != elem2:
+                count += 1.0
+    return count
 
 if __name__ == '__main__':
-    prueba = (((2,3,4,5),
+    prueba = ((2,3,4,5),
               (1,2,3,4),
               (9,10,12,13),
-              (15,0,34,5)))
+              (15,0,34,5))
+    
+    prueba2 = ((2, 3, 4, 8),
+               (1, 5, 7, 11),
+               (9, 6, 12, 0),
+               (13, 14, 10, 15))
     
     print(goal_test(prueba))
+    print(heuristic(prueba2))
