14411a48b033c831f9c1416b85941afc15626e5f
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2023-10-11 20:10:05 -0600

    Inicio de tarea 5 de criptoaritmetica

---
+++ 6.1 CSP/cryptarithmetic_puzzle.py
@@ -0,0 +1,24 @@
+#----------------------------------------------------------
+# Lab #5: Cryptarithmetic with CSP
+# General cryptarithmetic puzzle solver.
+#
+# Date: 13-Oct-2023
+# Authors:
+#           A01747869 Gustavo Gutiérrez
+#           A01746219 Eric Navarro
+#----------------------------------------------------------
+
+
+from csp import Constraint, CSP
+from typing import Optional
+
+
+def solve_cryptarithmetic_puzzle(
+        addends: list[str],
+        answer: str) -> Optional[dict[str, int]]:
+    # The rest of the function's code goes here
+    ...
+
+
+# The rest of the module's code goes here
+...
